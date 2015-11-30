f2d2b3d81e4853b62c86ab2c7d8ae63f37d4b311
Maxim Borisyak <mborisyak@yandex-team.ru>
2015-11-30 23:19:06 +0300

    ...

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def main():
   plt.savefig("precision-recall.png")
 
   plt.figure()
-  plt.boxplot(precision.T, positions=noise)
+  plt.boxplot(precision.T)
   plt.ylim([0, 1.1])
   plt.xlabel("track noise std")
   plt.ylabel("precision (truly recognized among recognized)")
@@ -111,11 +111,68 @@ def main():
   plt.savefig("precision.png")
 
   plt.figure()
-  plt.boxplot(recall.T, positions=noise)
+  plt.boxplot(recall.T)
   plt.ylim([0, 1.1])
   plt.xlabel("track noise std")
   plt.ylabel("recall (truly recognized among true tracks)")
   plt.title("Retina grid (%d x %d), particles = %d" % (bins, bins, particles_n))
   plt.savefig("recall.png")
 
-main()
+# main()
+
+def plot_event(event, track_ns=None):
+  from mpl_toolkits.mplot3d import Axes3D
+
+  fig = plt.figure(num=None, figsize=(16, 8), dpi=400)
+  ax = fig.add_subplot(111, projection='3d')
+
+  ax.scatter(event[:, 2], event[:, 0], event[:, 1], color="b", alpha=0.25, s = 1.0)
+
+  if track_ns is not None:
+      for i in range(track_ns.shape[0]):
+          (nx, ny, nz) = track_ns[i, :]
+          ax.plot([0, nz], [0, nx], [0, ny], c = "r", linewidth=1.0, alpha=0.25)
+
+  ax.set_xlabel("z")
+  ax.set_ylabel("x")
+  ax.set_zlabel("y")
+  #ax.set_ylim([-150, 150])
+  #ax.set_zlim([-150, 150])
+
+def main2():
+  for _ in range(10):
+    event, params, ns = simulation.particles(particles_n, np.arange(0, 20), theta_generation_limits,
+                                             trace_probability=0.75, trace_noise=0.05, detector_noise_rate=0.0)
+
+    #from pyretina.plot3d import plot_event
+
+    thetas, phis, response = retina_grid(event, theta_limits, bins, phi_limits, bins, sigma=0.01)
+    m = np.where(maxima(response))
+    predicted = np.vstack([ thetas[m], phis[m] ]).T
+
+    max_angle = 8 * spherical_angle(np.array([0.0, 0.0]), np.array([
+      (theta_limits[1] - theta_limits[0]) / bins, (phi_limits[1] - phi_limits[0]) / bins
+    ]))
+
+    _, predicted_mapping, test_mapping = binary_metrics(predicted, params, max_angle=max_angle)
+
+
+    print np.mean(predicted[predicted_mapping == 0], axis=0)
+
+    recognized = predicted_mapping == 1
+    test_recognized = test_mapping == 1
+    ghost = predicted_mapping == 0
+    unrecognized = test_mapping == 0
+
+    print m
+
+    plt.figure()
+    plt.scatter(thetas[m[0][ghost], m[1][ghost]], response[m[0][ghost], m[1][ghost]], marker="x", color="red")
+    plt.scatter(thetas[m[0][recognized], m[1][recognized]], response[m[0][recognized], m[1][recognized]], marker="+", color="green")
+    plt.plot()
+
+    plot_event(event)
+
+    plot_retina_results(predicted, params, thetas, phis, response, max_angle).show()
+
+main2()
